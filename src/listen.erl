4eb26c9207dce977e61fece91e82b1da991dd318
Dave Kujawski <dave.kujawski@gmail.com>
2013-03-02 13:57:30 +1300

    update modules to use native erlang message passing

--- src/listen.erl
+++ src/listen.erl
@@ -1,37 +1,16 @@
 -module(listen).
--include_lib("amqp_client/include/amqp_client.hrl").
--export([start/0]).
+-export([start/0, listener/0]).
 
 start() ->
-    {Channel, Queue} = init_channel_queue(),
     io:format(" [*] Waiting for messages. To exit press CTRL+C~n"),    
-    amqp_channel:subscribe(Channel, #'basic.consume'{queue = Queue, 
-                                                     no_ack = true}, self()),
-    receive
-        #'basic.consume_ok'{} -> ok
-    end,
-    loop(Channel).
+    register(listener, spawn(listen, listener, [])).
 
-loop(Channel) ->
+listener() ->
     receive
-        {#'basic.deliver'{routing_key = RoutingKey}, #amqp_msg{payload = Body}} ->
-            io:format(" [x] ~p:~w ", [RoutingKey, Body]),
-            fizzbazz:eval(Body),
-            loop(Channel)
+        finished ->
+            io:format("listener finished ~n", []);
+        {sender, Message} ->
+            io:format(" [x] : ~w ", [Message]),
+            fizzbazz:eval(Message)
     end.
 
-init_channel_queue() ->
-    Host = "localhost",
-    Exchange = <<"all">>,
-    RoutingKey = <<"X.USER.*">>,
-    {ok, Connection} =
-        amqp_connection:start(#amqp_params_network{host = Host}),
-    {ok, Channel} = amqp_connection:open_channel(Connection),
-    amqp_channel:call(Channel, #'exchange.declare'{exchange = Exchange, 
-                                                   type = <<"topic">>}),
-    #'queue.declare_ok'{queue = Queue} =
-        amqp_channel:call(Channel, #'queue.declare'{exclusive = true}),
-    amqp_channel:call(Channel, #'queue.bind'{exchange = Exchange,
-                                              routing_key = RoutingKey,
-                                              queue = Queue}),
-    {Channel, Queue}.
--- src/send.erl
+++ src/send.erl
@@ -1,33 +1,15 @@
 -module(send).
--include_lib("amqp_client/include/amqp_client.hrl").
--export([loop/0]).
+-export([start/0, sender/0]).
 
-loop() ->
+start() ->
+    spawn(send, sender, []).
+
+sender() ->
     RNum = random:uniform(100),
-    {Route, Message} = {<<"X.USER.dkujawski">>, <<RNum>>},
-    {Connection, Channel, Exchange} = init_conn_channel(),
-    send_message({Channel, Exchange, Route, Message}),
-    close_conn_channel({Connection, Channel}),
-    io:format(" [x] Sent ~p: ~w~n", [Route, Message]),
+
+    listener ! {sender, RNum},
+
+    io:format(" [x] Sent : ~w~n", [RNum]),
     timer:sleep(5000),
-    loop().
+    sender().
 
-init_conn_channel() ->
-    Host = "localhost",
-    Exchange = <<"all">>,
-    {ok, Connection} = 
-        amqp_connection:start(#amqp_params_network{host = Host}),
-    {ok, Channel} = amqp_connection:open_channel(Connection),
-    amqp_channel:call(Channel, #'exchange.declare'{exchange = Exchange, 
-                                                   type = <<"topic">>}),
-    {Connection, Channel, Exchange}.
-
-send_message({Channel, Exchange, Route, Message}) ->
-    amqp_channel:cast(Channel, 
-                      #'basic.publish'{exchange = Exchange, 
-                                       routing_key = Route},
-                      #amqp_msg{payload = Message}).
-
-close_conn_channel({Connection, Channel}) ->
-    ok = amqp_channel:close(Channel),
-    ok = amqp_connection:close(Connection).
