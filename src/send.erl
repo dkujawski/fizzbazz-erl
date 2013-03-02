118bfd1d4310889505057f46978438facc345d81
Dave Kujawski <dave.kujawski@gmail.com>
2013-03-02 22:20:10 +1300

    node name should be an atom not a string.

--- src/send.erl
+++ src/send.erl
@@ -1,8 +1,12 @@
 -module(send).
--export([start/0, sender/0]).
+-export([start/0, start/1, sender/0, sender/1]).
 
 start() ->
     spawn(send, sender, []).
+
+start([ListenerNode]) ->
+    io:format("~p~n", [ListenerNode]),
+    spawn(send, sender, [ListenerNode]).
 
 sender() ->
     RNum = random:uniform(100),
@@ -13,3 +17,11 @@ sender() ->
     timer:sleep(5000),
     sender().
 
+sender(ListenerNode) ->
+    RNum = random:uniform(100),
+
+    {listener, list_to_atom(ListenerNode)} ! {sender, RNum},
+
+    io:format(" [x] Sent : ~w~n", [RNum]),
+    timer:sleep(5000),
+    sender(ListenerNode).
